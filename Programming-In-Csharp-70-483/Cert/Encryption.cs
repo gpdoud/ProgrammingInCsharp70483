73ac18488258236ef552aa10bf87cc9729eae75b
Greg Doud <gpdoud@gmail.com>
2018-09-07 16:09:10 -0400

    Added Encription

---
+++ Programming-In-Csharp-70-483/Cert/Encryption.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cert {
+
+    class Encryption {
+
+        static byte[] key = Encoding.Default.GetBytes(new Guid().ToString().Substring(0, 16));
+        static byte[] iv = Encoding.Default.GetBytes(new Guid().ToString().Substring(0, 16));
+        static string clearText = "This cert test is hard!";
+        static byte[] cypherText;
+        static string encryptedText = String.Empty;
+
+        public static void Encrypt() {
+            using(var r = Rijndael.Create()) {
+                r.Key = key;
+                r.IV = iv;
+                ICryptoTransform encryptor = r.CreateEncryptor(key, iv);
+                using(var ms = new MemoryStream()) {
+                    using(var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write)) {
+                        using(var writer = new StreamWriter(cs)) {
+                            writer.Write(clearText);
+                        }
+                        cypherText = ms.ToArray();
+                    }
+                }
+            }
+        }
+        public static void Decrypt() {
+            using(var r = Rijndael.Create()) {
+                /*
+                 * When creating the encryptor or decryptor, the key and iv
+                 * are required
+                 */
+                ICryptoTransform decryptor = r.CreateDecryptor(key, iv);
+                using(var ms = new MemoryStream(cypherText)) {
+                    using(var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read)) {
+                        using(var reader = new StreamReader(cs)) {
+                            encryptedText = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+        public static void Start() {
+            Encrypt();
+            Decrypt();
+        }
+    }
+}
--- Programming-In-Csharp-70-483/Program.cs
+++ Programming-In-Csharp-70-483/Program.cs
@@ -8,29 +8,32 @@ namespace Programming_In_Csharp_70_483 {
 	class Program {
 
 		void Run() {
-			/* Threads */
-			//Chapter1.L0101_Creating_A_Thread.Start();
-			//Chapter1.L0102_Using_A_Background_Thread.Start();
-			//Chapter1.L0103_Parameterized_ThreadStart.Start();
-			//Chapter1.L0104_Stopping_A_Thread.Start();
-			//Chapter1.L0105_Using_ThreadStaticAttribute.Start();
-			//Chapter1.L0106_Using_ThreadLocal.Start();
-			//Chapter1.L0107_Queuing_Work_To_Thread_Pool.Start();
-			/* Tasks */
-			//Chapter1.L0108_Starting_A_Task.Start();
-			//Chapter1.L0109_Using_A_Task_That_Returns_A_Value.Start();
-			//Chapter1.L0110_Continuation_After_Task.Start();
-			//Chapter1.L0111_Schedule_Different_Continuation_Tasks.Start();
-			/* Events */
-			//Chapter1.L0185_Custom_Event_Accessor.Start();
-			Chapter1.L0187_Manually_Raising_Events_With_Exceptions_Handling.Start();
+            /* Threads */
+            //Chapter1.L0101_Creating_A_Thread.Start();
+            //Chapter1.L0102_Using_A_Background_Thread.Start();
+            //Chapter1.L0103_Parameterized_ThreadStart.Start();
+            //Chapter1.L0104_Stopping_A_Thread.Start();
+            //Chapter1.L0105_Using_ThreadStaticAttribute.Start();
+            //Chapter1.L0106_Using_ThreadLocal.Start();
+            //Chapter1.L0107_Queuing_Work_To_Thread_Pool.Start();
+            /* Tasks */
+            //Chapter1.L0108_Starting_A_Task.Start();
+            //Chapter1.L0109_Using_A_Task_That_Returns_A_Value.Start();
+            //Chapter1.L0110_Continuation_After_Task.Start();
+            //Chapter1.L0111_Schedule_Different_Continuation_Tasks.Start();
+            /* Events */
+            //Chapter1.L0185_Custom_Event_Accessor.Start();
+            //Chapter1.L0187_Manually_Raising_Events_With_Exceptions_Handling.Start();
 
-			/* Practice */
-			//Chapter1.Practice.O0104_CreatingAndUsingADelegate.Start();
-			//Chapter1.Practice.O0104_MulticastingDelegates.Start();
-			//Chapter1.Practice.O0104_UsingBuildInCalculateDelegate.Start();
-			//Chapter1.Practice.O0104_DefiningAndRaisingEventsUsingAction.Start();
-			//Chapter1.Practice.O0104_EventsUsingEventType.Start();
+            /* Practice */
+            //Chapter1.Practice.O0104_CreatingAndUsingADelegate.Start();
+            //Chapter1.Practice.O0104_MulticastingDelegates.Start();
+            //Chapter1.Practice.O0104_UsingBuildInCalculateDelegate.Start();
+            //Chapter1.Practice.O0104_DefiningAndRaisingEventsUsingAction.Start();
+            //Chapter1.Practice.O0104_EventsUsingEventType.Start();
+
+            /* Cert */
+            Cert.Encryption.Start();
 		}
 		static void Main(string[] args) {
 			(new Program()).Run();
